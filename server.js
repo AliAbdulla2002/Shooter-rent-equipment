0f01c7d96613e4e1a8172e0e066c8c857c808bb5
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-07-20 23:52:45 +0300

    Re-add dependencies and controllers and some folder by plans becauese some problem connections

--- server.js
+++ server.js
@@ -4,74 +4,25 @@
 // Remove these two lines if your regular DNS works correctly.
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-require("dotenv").config();
+const dotenv = require("dotenv")
+dotenv.config()
+const express = require("express")
+const app = express()
 
-const authCtrl = require("./controllers/auth.js");
-const isSignedIn = require("./middleware/is-signed-in.js");
-const passUserToView = require("./middleware/pass-user-to-view.js");
-const methodOverride = require("method-override");
-const { MongoStore } = require("connect-mongo");
-const upload = require("./config/multer");
-const session = require("express-session");
-const mongoose = require("mongoose");
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+const morgan = require("morgan")
+const session = require('express-session')
+const { MongoStore } = require('connect-mongo')
+const upload = require('./config/multer')
 
-const equipmentCtrl = require("./controllers/equipment.js");
+const isSignedIn = require('./middleware/is-signed-in')
+const passUserToView = require('./middleware/pass-user-to-view')
 
-const app = express();
+const authCtrl = require('./controllers/auth')
+const equipmentCtrl = require('./controllers/equipment')
 
-const PORT = process.env.PORT || 3000; // For me, this is best practice. Usually, the port is placed in .env; if not, 3000 will be used
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(morgan("dev"));
-
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({
-            mongoUrl: process.env.MONGODB_URI,
-        }),
-    })
-);
-
-app.use(passUserToView);
-
-app.get("/", authCtrl.home);
-app.get("/auth/sign-up", authCtrl.showSignUpForm);
-app.post("/auth/sign-up", authCtrl.signUp);
-app.get("/auth/sign-in", authCtrl.showSignInForm);
-app.post("/auth/sign-in", authCtrl.signIn);
-app.delete("/auth/sign-out", authCtrl.signOut);
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000"
 
 
-// all CRUD Route
-app.get("/equipment", equipmentCtrl.index);
-app.get("/equipment/new", isSignedIn, equipmentCtrl.renderNewForm);
-app.get("/equipment/:equipmentId", equipmentCtrl.show);
-app.get("/equipment/:equipmentId/edit", isSignedIn, equipmentCtrl.edit);
-app.post("/equipment", isSignedIn, upload.single("image"), equipmentCtrl.createEquipment);
-app.put("/equipment/:equipmentId", isSignedIn, upload.single("image"), equipmentCtrl.update);
-app.delete("/equipment/:equipmentId", isSignedIn, equipmentCtrl.deleteEquipment);
-
-
-const startServer = async function () {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-
-        console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
-
-        app.listen(PORT, function (){
-            console.log(`Listening on ${PORT}`);
-        });
-    } catch (error) {
-        console.log("MongoDB connection error:", error.message);
-    }
-};
-
-startServer();
