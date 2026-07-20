5538888f7bfbcc8da3c2845fe244b36e5c3bc043
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-07-21 00:26:34 +0300

    Delete the redandant file by mistake and trying to catch the error by using lecture auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,105 +1,74 @@
-﻿const User = require("../models/user");
-const bcrypt = require("bcrypt");
+﻿const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
-const home = function (req, res){
-    res.render("home.ejs", {
-        user: req.session.user,
-    });
-};
+const showSignUpForm = function (req, res) {
+    res.render('auth/sign-up.ejs')
+}
 
-const showSignUpForm = function (req, res){
-    res.render("auth/sign-up.ejs", {
-        user: req.session.user,
-    });
-};
-
-const signUp = async function (req, res){
-    if (req.body.password !== req.body.confirmPassword) {
-        return res.send("Passwords do not match!");
-    }
-
+const signUp = async function (req, res) {
     const userInDatabase = await User.findOne({
-        username: req.body.username,
-    });
+        username: req.body.username
+    })
 
     if (userInDatabase) {
-        return res.send("Username is already taken");
+        return res.send('Username already taken.')
     }
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    let userData = {}
+    userData.username = req.body.username
 
-    const userData = {
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-        role: req.body.role
-    };
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    userData.password = hashedPassword
 
-    const user = await User.create(userData);
+    const user = await User.create(userData)
 
     req.session.user = {
         username: user.username,
-        id: user.id,
-        role: user.role
-    };
-
+        _id: user._id
+    }
     req.session.save(function (){
-        res.redirect("/");
-    });
-};
+        res.redirect('/')
+    })
+}
 
-const showSignInForm = function (req, res){
-    res.render("auth/sign-in.ejs", {
-        user: req.session.user,
-    });
-};
+const showSignInForm = function (req, res) {
+    res.render('auth/sign-in.ejs')
+}
 
-const signIn = async function (req, res){
+const signIn = async function (req, res) {
     const userInDatabase = await User.findOne({
-        username: req.body.username,
-    });
+        username: req.body.username
+    })
 
     if (!userInDatabase) {
-        return res.send("User does not exist");
+        return res.send('User does not exist.')
     }
 
-    const validPassword = await bcrypt.compare(
-        req.body.password,
-        userInDatabase.password
-    );
+    const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password)
 
-    if (!validPassword) {
-        return res.send("Login failed");
+    if(!validPassword) {
+        return res.send('Login failed. Please try again.')
     }
 
     req.session.user = {
         username: userInDatabase.username,
-        id: userInDatabase.id,
-    };
+        _id: userInDatabase._id
+    }
+    req.session.save(function () {
+        res.redirect('/')
+    })
+}
 
-    req.session.save(function (){
-        res.redirect("/");
-    });
-};
-
-const signOut = function (req, res){
-    req.session.destroy(function (){
-        res.redirect("/");
-    });
-};
-
-const dashboard = function (req, res){
-    res.render("dashboard.ejs", {
-        user: req.session.user,
-    });
-};
+const signOut = async function (req, res) {
+    req.session.destroy(function () {
+        res.redirect('/')
+    })
+}
 
 module.exports = {
-    home,
     showSignUpForm,
     signUp,
     showSignInForm,
     signIn,
     signOut,
-    dashboard,
-};
+}
