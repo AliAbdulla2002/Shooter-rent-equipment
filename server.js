c3a8da6880a65d833b4142ec34bd0acb9ed319a1
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-07-22 20:47:16 +0300

    Finalize and test the project

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 ﻿const dns = require("node:dns");
-// DNS workaround for MongoDB Atlas.
-// Remove these two lines if your regular DNS works correctly.
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const dotenv = require("dotenv")
@@ -29,7 +27,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const bookingCtrl = require('./controllers/booking')
 
-// Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000"
 
 
@@ -60,24 +57,22 @@ app.get('/', function (req, res) {
     })
 })
 
-// AUTH ROUTERS
 app.get('/auth/sign-up', authCtrl.showSignUpForm )
 app.post('/auth/sign-up', authCtrl.signUp)
 app.get('/auth/sign-in', authCtrl.showSignInForm)
 app.post('/auth/sign-in', authCtrl.signIn)
 app.delete('/auth/sign-out', authCtrl.signOut)
 
-// EQUIPMENT ROUTERS
-app.get('/equipment/new', isSignedIn, isOwner, equipmentCtrl.showNewForm) // the update check the role first
+app.get('/equipment/new', isSignedIn, isOwner, equipmentCtrl.showNewForm)
 app.post('/equipment', isSignedIn, isOwner ,upload.single('image'), equipmentCtrl.create)
 app.get('/equipment', equipmentCtrl.index)
-app.get('/equipment/favorites', isSignedIn, equipmentCtrl.myFavorites) // if the user clicked on the fav
+app.get('/equipment/favorites', isSignedIn, equipmentCtrl.myFavorites)
 app.get('/equipment/:equipmentId', isSignedIn, equipmentCtrl.show)
 app.delete('/equipment/:equipmentId', isSignedIn, equipmentCtrl.deleteEquipment)
 app.get('/equipment/:equipmentId/edit', isSignedIn, equipmentCtrl.edit )
 app.put('/equipment/:equipmentId', isSignedIn, upload.single('image'), equipmentCtrl.update)
 
-app.post('/equipment/:equipmentId/favorited-by/:userId', isSignedIn, equipmentCtrl.favorite) // from lecture if i dont have a time i will deleted
+app.post('/equipment/:equipmentId/favorited-by/:userId', isSignedIn, equipmentCtrl.favorite)
 app.delete('/equipment/:equipmentId/favorited-by/:userId', isSignedIn, equipmentCtrl.unfavorite)
 
 app.get('/bookings/my-bookings', isSignedIn, bookingCtrl.myBookings)
@@ -85,19 +80,18 @@ app.post('/equipment/:equipmentId/bookings', isSignedIn, bookingCtrl.createBooki
 
 app.get('/owner/dashboard', isSignedIn, isOwner, bookingCtrl.ownerDashboard)
 
-app.delete('/bookings/:bookingId', isSignedIn, bookingCtrl.cancelBooking) // to cancel the booking 
+app.delete('/bookings/:bookingId', isSignedIn, bookingCtrl.cancelBooking)
 
-app.put('/owner/bookings/:bookingId', isSignedIn, isOwner, bookingCtrl.updateBookingStatus) // to update booking status
+app.put('/owner/bookings/:bookingId', isSignedIn, isOwner, bookingCtrl.updateBookingStatus)
 
-app.get('/equipment/:equipmentId/delete', isSignedIn, equipmentCtrl.showDeleteConfirm) // to show the confrimation delete
+app.get('/equipment/:equipmentId/delete', isSignedIn, equipmentCtrl.showDeleteConfirm)
 
-// HOMEE
+
 app.get('/dashboard', isSignedIn, async function (req, res){
     res.render('dashboard.ejs')
 })
 
 
-// ADD THE  splat function ERROR LIKE LECTURE - Nabila
 app.get('/*splat', function (req, res) {
     res.render('error.ejs', {
         msg: 404
